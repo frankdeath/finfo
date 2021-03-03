2da5f9ef4ad1a2533df7b4bae72d40536207bba1
Kevin Peterson <frankdeath@gmail.com>
2021-03-02 21:54:24 -0600

    Eliminate the need for a file existence function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
+use std::path::Path;
 extern crate clap;
 use clap::{Arg, App, AppSettings, SubCommand};
-
-fn file_exists(s: &str) -> bool {
-    println!("fileExists: {}", s);
-    true
-}
 
 fn main() {
     let matches = App::new("File Info (finfo)")
@@ -29,22 +25,18 @@ fn main() {
                                           .help("Name of file")))
                          .get_matches();
 
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
     if let Some(matches) = matches.subcommand_matches("sum") {
             let filename = matches.value_of("FILENAME").unwrap();
-            if file_exists(filename) {
-                // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-                // required we could have used an 'if let' to conditionally get the value)
-                println!("Calculating shasums of {}", filename);
+            let filepath = Path::new(filename);
+            if filepath.exists() {
+                println!("Calculating shasums of {:?}", filepath);
             }
     }
     if let Some(matches) = matches.subcommand_matches("time") {
             let filename = matches.value_of("FILENAME").unwrap();
-            if file_exists(filename) {
-                // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-                // required we could have used an 'if let' to conditionally get the value)
-                println!("Getting times of {}", filename);
+            let filepath = Path::new(filename);
+            if filepath.exists() {
+                println!("Getting times of {:?}", filepath);
             }
     }
 
